1c3eb28cbedcd976c4237fec803c6aa98d8c42d9
Hector Plinio <hectorplinio@MacBook-Air-de-Hector.local>
2021-11-30 12:32:45 +0100

    Express funcionando

---
+++ api/routes/users.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+const {v4 : uuid} = require ("uuid");
+let users = [
+  {
+      id: uuid(),
+      username: 'Hector',
+      comment: 'Es de Villena'
+  },
+  {
+      id: uuid(),
+      username: 'Alfonso',
+      comment: 'Es de Cañada'
+  },
+  {
+      id: uuid(),
+      username: 'Angel',
+      comment: 'Es de Novelda'
+  }
+];
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.render('users', { users });
+});
+
+module.exports = router;
---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react"
+
+function App() {
+    const zeroFill = n => {
+        return ('0' + n).slice(-2);
+    }
+    const interval = setInterval(() => {
+        // Pega o horário atual
+        const now = new Date();
+
+        // Formata a data conforme dd/mm/aaaa hh:ii:ss
+        const dataHora =  zeroFill(now.getHours()) + ':' + zeroFill(now.getMinutes()) + ':' + zeroFill(now.getSeconds());
+
+        // Exibe na tela usando a div#data-hora
+        document.getElementById('data-hora').innerHTML = dataHora;
+    }, 1000);
+    return (<div>
+        <h1>App prueba webpack Hector</h1>
+        <h3>Date : {new Date().toDateString()}</h3>
+        <h3>Time actulized :<div id="data-hora"></div></h3>
+        
+    </div>)
+}
+
+export default App
